911f1835bbf7c2c3f0b51ecfb80237062fd0cda4
yangqiuyi <732308967@qq.com>
2016-08-09 09:11:58 +0800

    修改evenbus错误类型

--- app/src/main/java/com/example/dell/newitsme/event/TurtleEventType.java
+++ app/src/main/java/com/example/dell/newitsme/event/TurtleEventType.java
@@ -4,13 +4,6 @@ package com.example.dell.newitsme.event;
 public class TurtleEventType {
 
    public static final int TYPE_HOT_DATA_OK = 0;//热门的数据到了
-   public static final int TYPE_API_DATA_OK = 0;//api接口拉到了
-   // TYPE_HOT_DATA_ERR  1
-   //
-
-
-
-
-
+   public static final int TYPE_API_DATA_OK = 1;//api接口拉到了
 
 }
